4c27d80dc60d0041fa2afe76f995bc678621c742
1050669722 <1050669722@qq.com>
2024-07-04 03:20:50 +0800

    Modify install.bat;

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @REM conda create --name LookUpWordsInstallationEnv --file environment.yml -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
 @REM conda activate LookUpWordsInstallationEnv
 
-@REM pyinstaller -F main.py --noconsole -n ProcessStringFromYoudaoDict
+@REM pyinstaller -F main.py --noconsole -n LookUpWords
 pyinstaller -F main.py -n LookUpWords
